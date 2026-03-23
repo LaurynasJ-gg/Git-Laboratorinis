0683d6d6e9aa6aaba2af68121f679917be3f41f4
Laurynas Jastrižemskis <laurynas.jastrizemskis@stud.svako.lt>
2026-03-23 15:10:39 +0200

    antra versija git.php kodo

--- git.php
+++ git.php
@@ -1,16 +1,51 @@
 <?php
-$a = 34;
-$i = 1;
-
-while ($a != 1) {
-	print "zingsnis $i - $a <br>";
-	if($a % 2 == 0) {
-		$a = $a / 2;
-	} else {
-		$a = 3 * $a + 1;
+	class uzduotis {
+	// kintamieji
+	public $skaicius;
+	public $i;
+	public $seka;
+	public $pskaicius;
+	
+	// duomenu ivedimas
+	
+	function ivestis($skaicius) {
+		$this->pskaicius = $skaicius;
+		$this->skaicius = $skaicius;
 	}
-	$i++;
-}
-print "zingsnis $i - $a<br>"; // reikia nes paskutinio zingsnio nespausdina
-print("atsakymas isviso - " . $i);
+
+	
+	// metodas skaiciuoti 3x+1
+	function skaiciuoti() {
+		$this->seka = "";
+		$this->i = 1;
+		$this->seka = $this->skaicius;
+		
+		while($this->skaicius != 1) {
+			if ($this->skaicius % 2 == 0) {
+				$this->skaicius = $this->skaicius / 2;
+			}
+			else {
+				$this->skaicius = 3 * $this->skaicius + 1;
+			}
+			$this->seka .= "<br>" . $this->skaicius;
+			$this->i++;
+		}
+	}
+	
+	// seka,zingsniai ir rezultatai
+	function seka() {
+		return $this-> seka;
+	}
+	
+	function zingsniai() {
+		return $this-> i;
+	}
+	
+	
+	function rez() {
+		print ("ivestas skaicius = " . $this->pskaicius . "<br>");
+		print ("zingsniai = " . $this->i . "<br>");
+		print ("seka = " . $this->seka);
+	}
+}	
 ?>
